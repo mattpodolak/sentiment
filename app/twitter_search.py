69d5b0201ec9657b815aa8ded9e7f7cbd01136b0
Jason Gomez <jason@enlightme.ca>
2018-08-25 14:34:41 -0400

    news_search

--- app/twitter_search.py
+++ app/twitter_search.py
@@ -1,22 +1,13 @@
-from TwitterSearch import *
-
-try:
-    tso = TwitterSearchOrder() # create a TwitterSearchOrder object
-    tso.set_keywords(['Tims']) # let's define all words we would like to have a look for
-    tso.set_language('en') # we want to see German tweets only
-    tso.set_include_entities(False) # and don't give us all those entity information
-
-    # it's about time to create a TwitterSearch object with our secret tokens
-    ts = TwitterSearch(
-        consumer_key = 'aaabbb',
-        consumer_secret = 'cccddd',
-        access_token = '111222',
-        access_token_secret = '333444'
-    )
-
-    # this is where the fun actually starts :)
-    for tweet in ts.search_tweets_iterable(tso):
-        print( '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ) )
-
-except TwitterSearchException as e: # take care of all those ugly errors if there are some
-    print(e)
+import requests
+def news_search(keyword):
+    url = ('https://newsapi.org/v2/everything?'
+        'q='+keyword+'&'
+        'from=2018-08-20&'
+        'sortBy=popularity&'
+        'apiKey=a81c4da648bd4945ba2a4d15413ff608')
+    response = requests.get(url)
+    r = response.json()
+    #print(r)
+    print(r['totalResults'])
+    for titles in r['articles']:
+        print(titles['title'])
