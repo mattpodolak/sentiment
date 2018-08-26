26d6dc140946f381e462d0725af799d1dc24e130
Jason Gomez <jason@enlightme.ca>
2018-08-26 01:32:49 -0400

    api keys

--- app/search.py
+++ app/search.py
@@ -6,7 +6,7 @@ def news_search(keyword, date_str):
         'to='+date_str+'&'
         'language=en&'
         'sortBy=popularity&'
-        'apiKey=b33925218d90423a9e742920006e9c75')
+        'apiKey=2fe31a4662ee456b9c2180ee36ef62ee')
     response = requests.get(url)
     r = response.json()
     title_array = []
